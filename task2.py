1a76da65b0a8361c8749288a753a168dd8657c76
pollle4kaa <125691054+pollle4kaa@users.noreply.github.com>
2024-02-26 17:24:32 +0300

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,16 +1,12 @@
 import csv
 all_sc = [] #дубликат таблицы
 with open ('scientist.txt','r',encoding='utf-8') as file:
-    with open ('scientist_origin.txt','w',encoding='utf-8',newline='') as filenew:
-        data = file.read().split('\n')[:-1]
-        headline = data.pop(0).split('#')
-        w = csv.writer(filenew)
-        w.writerow(headline)
-        for i in data:
-            s = i.split('#')
-            all_sc.append(s)
-        all_sc.sort(key = lambda x: x[2])
-        for i in range (5):
-            scien = all_sc[i][0]
-            prep = all_sc[i][1]
-            print(f'{scien}: {prep}')
+    data = file.read().split('\n')[1:-1]
+    for i in data:
+        s = i.split('#')
+        all_sc.append(s)
+    all_sc.sort(key = lambda x: x[2])
+    for i in range (5):
+        scien = all_sc[i][0]
+        prep = all_sc[i][1]
+        print(f'{scien}: {prep}')
